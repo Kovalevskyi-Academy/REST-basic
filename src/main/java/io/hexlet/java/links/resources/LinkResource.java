b56ec84722451bfe97870ff2902f4306e05acaea
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2017-08-31 21:07:04 -0700

    Version with mongo

--- src/main/java/io/hexlet/java/links/resources/LinkResource.java
+++ src/main/java/io/hexlet/java/links/resources/LinkResource.java
@@ -1,19 +1,29 @@
 package io.hexlet.java.links.resources;
 
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Iterator;
+import java.util.Random;
 
 // my.site.com/api/links
 @Path("links")
 public class LinkResource {
 
-    private static final AtomicInteger currentId = new AtomicInteger();
+    private static final MongoCollection<Document> LINKS_COLLECTION;
 
-    private static final Map<String, String> links = new ConcurrentHashMap<>();
+    static {
+        final MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        final MongoDatabase db = mongo.getDatabase("hexlet");
+        LINKS_COLLECTION = db.getCollection("links");
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -22,8 +32,14 @@ public class LinkResource {
         if (id == null || id.isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        final String url = links.get(id);
-        if (url == null) {
+        final FindIterable<Document> resultsIterable
+                = LINKS_COLLECTION.find(new Document("id", id));
+        final Iterator<Document> resultIterator = resultsIterable.iterator();
+        if (!resultIterator.hasNext()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        final String url = resultIterator.next().getString("url");
+        if (url == null || url == "") {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(url).build();
@@ -33,8 +49,30 @@ public class LinkResource {
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     public Response shortUrl(final String url) {
-        final int id = currentId.getAndIncrement();
-        links.put(String.valueOf(id), url);
-        return Response.ok(id).build();
+        int attempt = 0;
+        while (attempt < 5) {
+            final String id = getRandomId();
+            final Document newShortDoc = new Document("id", id);
+            newShortDoc.put("url", url);
+            try {
+                LINKS_COLLECTION.insertOne(newShortDoc);
+                return Response.ok(id).build();
+            } catch (MongoWriteException e) {
+                // attempt to write failed, ID - exists.
+            }
+            attempt++;
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    private static String getRandomId() {
+        String possibleCharacters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+        StringBuilder idBuilder = new StringBuilder();
+        Random rnd = new Random();
+        while (idBuilder.length() < 5) {
+            int index = (int) (rnd.nextFloat() * possibleCharacters.length());
+            idBuilder.append(possibleCharacters.charAt(index));
+        }
+        return idBuilder.toString();
     }
 }
